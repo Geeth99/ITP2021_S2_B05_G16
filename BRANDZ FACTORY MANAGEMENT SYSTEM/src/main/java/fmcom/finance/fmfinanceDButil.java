67849e6143b9d4c7814997a3e10c3a50a18fd4ac
CHAMA COMPUTERS <CHAMA COMPUTERS@SHALUKA>
2021-09-11 23:00:18 +0530

    income records completed

---
+++ BRANDZ FACTORY MANAGEMENT SYSTEM/src/main/java/fmcom/finance/fmfinanceDButil.java
@@ -0,0 +1,48 @@
+package fmcom.finance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+
+public class fmfinanceDButil {
+	
+public static boolean incomeinsert(String name,String Bdetails,String discount,
+			String Icode,String quantity,String price ) {
+		
+		boolean isSuccess=false;
+		
+		//Create database connection
+		String url = "jdbc::mysql:://localhost::3306/finance";
+		String user ="root";
+		String pass ="S20232436!a";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+Connection con = (Connection) DriverManager.getConnection(url,user,pass);
+Statement stmt = con.createStatement();
+
+String sql ="insert into income values(0,'"+name+"','"+Bdetails+"','"+discount+"','"+Icode+"','"+quantity+"','"+price+"')";
+int rs = stmt.executeUpdate(sql);
+
+if(rs > 0) {
+	isSuccess= true;
+	}else {
+		isSuccess= false;
+	}
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		
+		return isSuccess;
+		
+		
+	}
+
+}
---
+++ BRANDZ FACTORY MANAGEMENT SYSTEM/src/main/webapp/fmIncomeInsert.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="#" method="post">
+	Buyer name<input type="text" name="Bname"><br>
+	Bank details<input type="text" name="Bank-details"><br>
+	Discount<input type="text" name="Disount"><br>
+	item code <input type="text" name="icode"><br>
+	Quantity<input type="text" name="quan"><br>
+	Price <input type="text" name="Price"><br>
+
+<input type="submit" name="submit" value="submit"> <button> cancel </button>
+ 
+</form>
+</body>
+</html>
